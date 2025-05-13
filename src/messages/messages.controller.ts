4154c681ede3672dea16bb87062a76751b993ef3
Abdulrazaq I. Ali <a.ali.t@scfhs.org.sa>
2025-05-13 13:31:47 +0300

    This is how we make body on post

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post,Body,Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,8 +8,9 @@ export class MessagesController {
     }
 
     @Post()
-    createMessage() {
-
+    createMessage(@Body() body: any ) {
+     console.log(body);
+        
     }
 
     @Get('/:id')
