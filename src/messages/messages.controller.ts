d2f6377ea6d7e1575201c09f44764bae7d955292
Abdulrazaq I. Ali <a.ali.t@scfhs.org.sa>
2025-05-14 09:16:18 +0300

    finish api

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,26 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { createMessageDto } from './DTOs/create-message.dto';
+import { MessagessService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messageService: MessagessService;
+    constructor(){
+        this.messageService = new MessagessService();
+    }
     @Get()
     listMessages() {
-
+        return this.messageService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: createMessageDto) {
-        console.log(body);
+       return this.messageService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        console.log(id);
+        return this.messageService.findOne(id);
 
     }
 }
